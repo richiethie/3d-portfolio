52fa1ac290837801d0848b23e3ffd12b474e1392
richiethie <richiethie@gmail.com>
2023-06-14 23:51:33 -0500

    rendered 3d pc model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Richie</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className='sm:block hidden' />interfaces and web applications.</p>
         </div>
 
       </div>
-
+      <ComputersCanvas />
     </section>
   )
 }
